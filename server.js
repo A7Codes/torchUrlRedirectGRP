29d8b834020e3eb9724c7f4134397f1734804358
A7Codes <A7codes@gmail.com>
2023-01-26 17:30:07 +0300

    second commit

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const http = require("http");
 const app = express();
 require("dotenv").config();
 const port = process.env.port;
+
+var httpServer = http.createServer(app);
+
 app.get("/", (req, res) =>{
 res.redirect("http://207.180.227.25:1211/baghdad/");
-})
+});
 
 app.listen(port);
+httpServer.listen(80);
